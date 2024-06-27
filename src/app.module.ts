2ecaa4489f85f1a07bf75590bb938d2fafbec688
Juan Pablo Victoria <44125388+juanp288@users.noreply.github.com>
2024-06-27 18:34:36 -0500

    feat: Added polloServerPluginLandingPage

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthorsModule, PostModule } from './modules';
 import { Post } from './modules/post/entities/post.entity';
 import { Author } from './modules/authors/entities/author.entity';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
     TypeOrmModule.forRoot({
       type: 'sqlite',
